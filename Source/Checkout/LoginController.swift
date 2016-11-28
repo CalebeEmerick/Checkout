863c7babfe2f99b87b5383db23527c416ed57b88
Calebe Emerick <calebe_emerick@hotmail.com>
2016-11-28 21:37:39 -0200

    Começando a criar a tela de login.

---
+++ Source/Checkout/LoginController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginController.swift
+//  Checkout
+//
+//  Created by Calebe Emerick on 28/11/16.
+//  Copyright © 2016 CalebeEmerick. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - LoginController -
+
+final class LoginController : UIViewController {
+
+    
+}
+
+// MARK: - Life Cycle -
+
+extension LoginController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
