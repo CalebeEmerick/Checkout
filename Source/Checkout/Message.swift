6521f795921c65aec0f72f9a9b42ed47f637d013
Calebe Emerick <calebe_emerick@hotmail.com>
2016-11-29 19:36:16 -0200

    Retirando mensagem desnecessárias.

--- Source/Checkout/Message.swift
+++ Source/Checkout/Message.swift
@@ -11,10 +11,4 @@ import Foundation
 struct Message {
     
     static let generalError = "Aconteceu algo inesperado. Por favor, tente novamente."
-    
-    struct Login {
-        
-        static let missingData = "Os campos Usuário e Senha são obrigatórios. Por favor, preencha-os e tente novamente."
-        static let wrongData = "Usuário ou Senha inválidos. Por favor, tente novamente."
-    }
 }
